1ebd239ed60260329405f0ad02bcb09c0cfcbab7
Grant Rodgers <grantr@gmail.com>
2011-02-11 11:57:42 -0800

    use @new_record instead of @persisted
    set it properly in instantiate

--- lib/cassandra_object/base.rb
+++ lib/cassandra_object/base.rb
@@ -69,7 +69,7 @@ module CassandraObject
 
     def initialize(attributes={})
       @key = attributes.delete(:key)
-      @persisted = false
+      @new_record = true
       @destroyed = false
       @attributes = {}.with_indifferent_access
       self.attributes = attributes
