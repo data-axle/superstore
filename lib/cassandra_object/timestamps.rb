89acfb8b80add5213e7b7fadbcc1a08df0d1e0a5
Grant Rodgers <grantr@gmail.com>
2011-05-25 17:14:19 -0700

    class attributes have instance accessors

--- lib/cassandra_object/timestamps.rb
+++ lib/cassandra_object/timestamps.rb
@@ -13,11 +13,11 @@ module CassandraObject
     end
 
     def set_created_at
-      self.created_at = Time.current unless self.class.timestamp_override
+      self.created_at = Time.current unless timestamp_override
     end
 
     def set_updated_at
-      self.updated_at = Time.current unless self.class.timestamp_override
+      self.updated_at = Time.current unless timestamp_override
     end
   end
 end
