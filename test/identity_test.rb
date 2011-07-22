dd8c8b7e7d835f24167dacf65d6406d80535371d
Matthew Higgins <developer@matthewhiggins.com>
2011-07-22 14:46:28 -0700

    add equality tests

--- test/identity_test.rb
+++ test/identity_test.rb
@@ -4,4 +4,12 @@ class CassandraObject::IdentiyTest < CassandraObject::TestCase
   test 'equality of new records' do
     assert_not_equal Issue.new, Issue.new
   end
+
+  test 'equality' do
+    first_issue = Issue.create
+    second_issue = Issue.create
+
+    assert_equal first_issue, first_issue
+    assert_not_equal first_issue, second_issue
+  end
 end
