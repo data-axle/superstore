4ead408365b7ecbdcf5a154a89386c656692ab9c
Matthew Higgins <developer@matthewhiggins.com>
2012-04-30 11:47:52 -0700

    fix a foolish piece of code that instantiates all attributes for each hashed attribute

--- lib/cassandra_object/identity/hashed_natural_key_factory.rb
+++ lib/cassandra_object/identity/hashed_natural_key_factory.rb
@@ -3,7 +3,7 @@ module CassandraObject
   module Identity
     class HashedNaturalKeyFactory < NaturalKeyFactory
       def next_key(object)
-        NaturalKey.new(Digest::SHA1.hexdigest(attributes.map { |a| object.attributes[a.to_s] }.join(separator)))
+        NaturalKey.new(Digest::SHA1.hexdigest(attributes.map { |attribute| object[attribute] }.join(separator)))
       end
     end
   end
