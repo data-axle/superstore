2e093176b91828e2a9500d41ce99e6e4948db45e
Matthew Higgins <developer@matthewhiggins.com>
2012-07-06 19:24:17 -0700

    automate keyspace and column family generation

--- test/support/connect.rb
+++ test/support/connect.rb
@@ -1,4 +1,12 @@
 CassandraObject::Base.establish_connection(
-  keyspace: 'place_directory_development',
+  keyspace: 'cassandra_object_test',
   servers: '127.0.0.1:9160'
 )
+
+CassandraObject::Tasks::Keyspace.new.tap do |keyspace_task|
+  keyspace_task.create('cassandra_object_test') unless keyspace_task.exists?('cassandra_object_test')
+end
+
+CassandraObject::Tasks::ColumnFamily.new('cassandra_object_test').tap do |column_family_task|
+  column_family_task.create('Issues') unless column_family_task.exists?('Issues')
+end
