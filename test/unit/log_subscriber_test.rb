76d507b34280042faddc34e7892b5a53be5be55a
Matthew Higgins <developer@matthewhiggins.com>
2012-08-13 14:15:43 -0700

    fix syntax of test

--- test/unit/log_subscriber_test.rb
+++ test/unit/log_subscriber_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 require "active_support/log_subscriber/test_helper"
 
-class CassandraObject::LogSubscriberTest < ActiveRecord::TestCase
+class CassandraObject::LogSubscriberTest < CassandraObject::TestCase
   include ActiveSupport::LogSubscriber::TestHelper
-  
+end
