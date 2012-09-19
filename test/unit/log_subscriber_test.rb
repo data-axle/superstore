7442be93d902195e4032760210a8cf9f44839375
Matthew Higgins <developer@matthewhiggins.com>
2012-09-19 14:51:20 -0700

    fill out log_subscriber_test.rb

--- test/unit/log_subscriber_test.rb
+++ test/unit/log_subscriber_test.rb
@@ -1,6 +1,22 @@
 require "test_helper"
 require "active_support/log_subscriber/test_helper"
+require "cassandra_object/log_subscriber"
 
 class CassandraObject::LogSubscriberTest < CassandraObject::TestCase
   include ActiveSupport::LogSubscriber::TestHelper
+
+  def setup
+    super
+
+    CassandraObject::LogSubscriber.attach_to :cassandra_object
+  end
+
+  def test_cql_notification
+    Issue.execute_cql "SELECT * FROM Issues"
+
+    wait
+
+    assert_equal 1, @logger.logged(:debug).size
+    assert_match "SELECT * FROM Issues", @logger.logged(:debug)[0]
+  end
 end
