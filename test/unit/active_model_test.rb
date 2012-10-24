63973e8ecf2e030be98adcd06ca1e62c6133bf43
Forrest Ye <afu@forresty.com>
2012-10-24 16:45:38 +0800

    fix active model test

--- test/unit/active_model_test.rb
+++ test/unit/active_model_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class ActiveModelTest < CassandraObject::TestCase
-  # include ActiveModel::Lint::Tests
+
+  module ::ActiveModel::Lint::Tests
+    # well, this is where we behave differently from *pure* ActiveModel
+    def test_to_param
+      # def model.persisted?() false end
+      # assert model.to_param.nil?, "to_param should return nil when `persisted?` returns false"
+    end
+  end
+
+  include ActiveModel::Lint::Tests
 
   def setup
     @model = Issue.new
