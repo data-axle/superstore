f1d4f46426821056dc4ace4fb4619b1516afb5b2
Matthew Higgins <developer@matthewhiggins.com>
2012-10-31 09:50:35 -0700

    remove # prefix from test name, define classes under TestCase because putting them in the block adds no more scoping

--- test/unit/belongs_to/reflection_test.rb
+++ test/unit/belongs_to/reflection_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class CassandraObject::BelongsTo::ReflectionTest < CassandraObject::TestCase
-  test '#class_name' do
-    class ::Status < CassandraObject::Base; end
-    class ::User < CassandraObject::Base
-      belongs_to :status
-    end
+  class ::Status < CassandraObject::Base; end
+  class ::User < CassandraObject::Base
+    belongs_to :status
+  end
 
+  test 'class_name' do
     assert_equal 'Status', User.new.belongs_to_reflections[:status].class_name
   end
 end
