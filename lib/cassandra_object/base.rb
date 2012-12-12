4bba700832cb6aa7f4657b18cedad54109475521
Matthew Higgins <developer@matthewhiggins.com>
2012-12-12 14:42:57 -0800

    also support if self == Base

--- lib/cassandra_object/base.rb
+++ lib/cassandra_object/base.rb
@@ -31,7 +31,7 @@ module CassandraObject
         # Returns the class descending directly from ActiveRecord::Base or an
         # abstract class, if any, in the inheritance hierarchy.
         def class_of_active_record_descendant(klass)
-          if klass.superclass == Base
+          if klass == Base || klass.superclass == Base
             klass
           elsif klass.superclass.nil?
             raise "#{name} doesn't belong in a hierarchy descending from CassandraObject"
