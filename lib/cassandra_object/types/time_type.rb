ebb3ab2309d39cc54eaf0cbe4cc2646d0fe45a03
Matthew Higgins <developer@matthewhiggins.com>
2013-07-18 13:28:05 -0700

    Just call Time.parse, rather than xmlschema parsing

--- lib/cassandra_object/types/time_type.rb
+++ lib/cassandra_object/types/time_type.rb
@@ -1,22 +1,15 @@
 module CassandraObject
   module Types
     class TimeType < BaseType
-      REGEX = /\A\s*
-                (-?\d+)-(\d\d)-(\d\d)
-                T
-                (\d\d):(\d\d):(\d\d)
-                (\.\d*)?
-                (Z|[+-]\d\d:\d\d)?
-                \s*\z/ix
-
       def encode(time)
         raise ArgumentError.new("#{time.inspect} is not a Time") unless time.kind_of?(Time)
         time.utc.xmlschema(6)
       end
 
       def decode(str)
-        return nil unless str && str.match(TimeType::REGEX)
-        Time.xmlschema(str).in_time_zone
+        Time.parse(str).utc if str
+      rescue
+
       end
     end
   end
--- test/unit/types/time_type_test.rb
+++ test/unit/types/time_type_test.rb
@@ -9,5 +9,6 @@ class CassandraObject::Types::TimeTypeTest < CassandraObject::Types::TestCase
     assert_nil coder.decode(nil)
     assert_nil coder.decode('bad format')
     assert_equal Time.utc(2004, 12, 24, 1, 2, 3), coder.decode('2004-12-24T01:02:03.000000Z')
+    assert_equal Time.utc(2013, 07, 18, 20, 12, 46), coder.decode('2013-07-18 13:12:46 -0700')
   end
 end
