4bb623a00726bc1fa16659f8085373ef418e5871
Neville Kadwa <neville@kadwa.com>
2013-08-14 15:52:37 -0400

    copy array dirty handling to json, fixes #42

--- test/unit/types/json_type_test.rb
+++ test/unit/types/json_type_test.rb
@@ -9,4 +9,19 @@ class CassandraObject::Types::JsonTypeTest < CassandraObject::Types::TestCase
   test 'decode' do
     assert_equal({'a' => 'b'}, coder.decode({'a' => 'b'}.to_json))
   end
+
+  test 'delete marks dirty' do
+    record = temp_object do
+      string :name
+      json :stuff
+
+    end.new name: 'abcd', stuff: Hash[a: 1, b: 2]
+
+    record.stuff.delete :b
+
+    assert record.stuff_changed?
+    assert_equal Hash[a: 1], record.stuff
+  end
+
+
 end
