07196e6afb7e09af55cab0d4863dac49af99cd9b
Matthew Higgins <developer@matthewhiggins.com>
2013-10-10 14:02:04 -0700

    Revert "copy array dirty handling to json, fixes #42"
    This reverts commit 4bb623a00726bc1fa16659f8085373ef418e5871.

--- lib/cassandra_object/types/json_type.rb
+++ lib/cassandra_object/types/json_type.rb
@@ -1,46 +1,6 @@
 module CassandraObject
   module Types
     class JsonType < BaseType
-      class DirtyHash < Hash
-        attr_accessor :record, :name, :options
-        def initialize(record, name, hash, options)
-          @record   = record
-          @name     = name.to_s
-          @options  = options
-
-          self.merge!(hash)
-        end
-
-        def []=(obj, val)
-          modifying do
-            super
-          end
-        end
-
-        def delete(obj)
-          modifying do
-            super
-          end
-        end
-
-        private
-        def modifying
-          unless record.changed_attributes.include?(name)
-            original = dup
-          end
-
-          result = yield
-
-          if !record.changed_attributes.key?(name) && original != self
-            record.changed_attributes[name] = original
-          end
-
-          record.send("#{name}=", self)
-
-          result
-        end
-      end
-
       def encode(hash)
         ActiveSupport::JSON.encode(hash)
       end
@@ -48,11 +8,6 @@ module CassandraObject
       def decode(str)
         ActiveSupport::JSON.decode(str)
       end
-
-      def wrap(record, name, value)
-        DirtyHash.new(record, name, Hash(value), options)
-      end
-
     end
   end
 end
