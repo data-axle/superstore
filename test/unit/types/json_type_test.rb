b4c6278768ab847526f1ff6073660c19f60cf5cb
Matthew Higgins <developer@matthewhiggins.com>
2013-10-10 14:12:27 -0700

    Revert "Revert "better implementation, Fixes #42 again""
    This reverts commit 5adb09a8ad908151d9b51c42c50854ac400a2673.

--- test/unit/types/json_type_test.rb
+++ test/unit/types/json_type_test.rb
@@ -10,6 +10,56 @@ class CassandraObject::Types::JsonTypeTest < CassandraObject::Types::TestCase
     assert_equal({'a' => 'b'}, coder.decode({'a' => 'b'}.to_json))
   end
 
+  test 'setting marks dirty' do
+    record = temp_object do
+      string :name
+      json :stuff
+
+    end.new name: 'abcd', stuff: Hash[a: 1, b: 2]
+
+    record.save!
+    assert !record.stuff_changed?
+
+    record.stuff[:c] = 3
+
+    assert record.stuff_changed?
+    assert_equal Hash[a: 1, b: 2, c: 3], record.stuff
+  end
+
+  test 'hash change marks dirty' do
+    record = temp_object do
+      string :name
+      json :stuff
+
+    end.new name: 'abcd', stuff: Hash[a: 1, b: 2, v: {}]
+
+    record.save!
+    assert !record.stuff_changed?
+
+    record.stuff[:v][:data] = 69
+    record.stuff[:v] = record.stuff[:v]
+
+    assert record.stuff_changed?
+    assert_equal Hash[a: 1, b: 2, v: Hash[data: 69]], record.stuff
+  end
+
+  test 'hash no change does not dirty' do
+    record = temp_object do
+      string :name
+      json :stuff
+
+    end.new name: 'abcd', stuff: Hash[a: 1, b: 2, v: {data: 69}]
+
+    record.save!
+    assert !record.stuff_changed?
+
+    record.stuff[:v][:data] = 69
+    record.stuff[:v] = record.stuff[:v]
+
+    assert !record.stuff_changed?
+    assert_equal Hash[a: 1, b: 2, v: Hash[data: 69]], record.stuff
+  end
+
   test 'delete marks dirty' do
     record = temp_object do
       string :name
