9ffbdba5abccd6f087b0ee96db435081f1fc2590
Matthew Higgins <developer@matthewhiggins.com>
2013-11-17 15:58:24 -0800

    No more dirty array

--- lib/cassandra_object/types/array_type.rb
+++ lib/cassandra_object/types/array_type.rb
@@ -1,75 +1,19 @@
 module CassandraObject
   module Types
     class ArrayType < BaseType
-      class DirtyArray < Array
-        attr_accessor :record, :name, :options
-        def initialize(record, name, array, options)
-          @record   = record
-          @name     = name.to_s
-          @options  = options
-
-          super(array)
-          setify!
-        end
-
-        def <<(obj)
-          modifying do
-            super
-            setify!
-          end
-        end
-
-        def delete(obj)
-          modifying do
-            super
-          end
-        end
-
-        private
-          def setify!
-            if options[:unique]
-              reject!(&:blank?)
-              uniq!
-              begin sort! rescue ArgumentError end
-            end
-          end
-
-          def modifying
-            unless record.changed_attributes.include?(name)
-              original = dup
-            end
-
-            result = yield
-
-            if !record.changed_attributes.key?(name) && original != self
-              record.changed_attributes[name] = original
-            end
-
-            record.send("#{name}=", self)
-
-            result
-          end
-      end
-
-      # def default
-      #   []
-      # end
-
       def encode(array)
         raise ArgumentError.new("#{array.inspect} is not an Array") unless array.kind_of?(Array)
         array.to_a.to_json
       end
 
       def decode(str)
-        return [] if str.blank?
+        return nil if str.blank?
 
-        ActiveSupport::JSON.decode(str).tap do |array|
-          array.uniq! if options[:unique]
-        end
+        ActiveSupport::JSON.decode(str)
       end
 
       def wrap(record, name, value)
-        DirtyArray.new(record, name, Array(value), options)
+        value
       end
     end
   end
--- test/unit/types/array_type_test.rb
+++ test/unit/types/array_type_test.rb
@@ -11,57 +11,7 @@ class CassandraObject::Types::ArrayTypeTest < CassandraObject::Types::TestCase
 
   test 'decode' do
     assert_equal ['1', '2'], coder.decode(['1', '2'].to_json)
-    assert_equal [], coder.decode(nil)
-    assert_equal [], coder.decode('')
-  end
-
-  class TestIssue < CassandraObject::Base
-    self.column_family = 'Issues'
-    array :favorite_colors, unique: true
-  end
-
-  test 'append marks dirty' do
-    issue = TestIssue.create favorite_colors: []
-    assert !issue.changed?
-
-    issue.favorite_colors << 'red'
-    assert issue.changed?
-    assert_equal({'favorite_colors' => [[], ['red']]}, issue.changes)
-  end
-
-  test 'delete marks dirty' do
-    issue = TestIssue.create favorite_colors: ['red']
-    assert !issue.changed?
-
-    issue.favorite_colors.delete('red')
-    assert issue.changed?
-    assert_equal({'favorite_colors' => [['red'], []]}, issue.changes)
-  end
-
-  test 'unique array removes blank' do
-    issue = TestIssue.create favorite_colors: ['blue', 'red', '', nil]
-    assert_equal ['blue', 'red'], issue.favorite_colors
-  end
-
-  test 'unique array uniquifies' do
-    issue = TestIssue.create favorite_colors: ['blue', 'red']
-
-    issue.favorite_colors = ['red', 'red', 'blue']
-    assert !issue.changed?
-
-    issue.favorite_colors = ['red', 'green']
-    assert issue.changed?
-  end
-
-  test 'unique array rescues argument error' do
-    issue = TestIssue.create favorite_colors: [1, 'red']
-
-    assert_equal [1, 'red'], issue.favorite_colors
-  end
-
-  test 'write non array' do
-    issue = TestIssue.create favorite_colors: true
-
-    assert_equal [true], issue.favorite_colors
+    assert_equal nil, coder.decode(nil)
+    assert_equal nil, coder.decode('')
   end
 end
