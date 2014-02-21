52cc979adc4ee85c03289defbcb0c5d8f57b1827
Matthew Higgins <developer@matthewhiggins.com>
2014-02-20 19:40:09 -0800

    Autoload cassandra.yml if exists

--- lib/cassandra_object/railtie.rb
+++ lib/cassandra_object/railtie.rb
@@ -4,6 +4,25 @@ module CassandraObject
       load 'cassandra_object/tasks/ks.rake'
     end
 
+    initializer "cassandra_object.log_runtime" do |app|
+      ActiveSupport.on_load :cassandra_object do
+        pathname = Rails.root.join('config', 'cassandra.yml')
+        if pathname.exist?
+          config = YAML.load(pathname.read)
+
+          if config = config[Rails.env]
+            self.config = {
+              keyspace: config['keyspace'],
+              servers: config['servers'],
+              thrift: config['thrift']
+            }
+          else
+            raise "Missing environment #{Rails.env} in cassandra.yml"
+          end
+        end
+      end
+    end
+
     # Expose database runtime to controller for logging.
     initializer "cassandra_object.log_runtime" do |app|
       require "cassandra_object/railties/controller_runtime"
