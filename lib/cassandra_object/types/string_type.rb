1cad504b3bf9fa48687c295243b74c0b49cd22af
Matthew Higgins <developer@matthewhiggins.com>
2014-04-10 12:02:30 -0700

    Only force encoding when necessary

--- lib/cassandra_object/types/string_type.rb
+++ lib/cassandra_object/types/string_type.rb
@@ -3,7 +3,12 @@ module CassandraObject
     class StringType < BaseType
       def encode(str)
         raise ArgumentError.new("#{str.inspect} is not a String") unless str.kind_of?(String)
-        (str.frozen? ? str.dup : str).force_encoding('UTF-8')
+
+        unless str.encoding == Encoding::UTF_8
+          (str.frozen? ? str.dup : str).force_encoding('UTF-8')
+        else
+          str
+        end
       end
     end
   end
