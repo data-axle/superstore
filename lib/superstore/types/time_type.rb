7b874fdb6a04b93e5ebed5e14da1dc01e5938b55
Matthew Higgins <developer@matthewhiggins.com>
2014-07-01 17:43:21 -0700

    Support DateTime arguments

--- lib/superstore/types/time_type.rb
+++ lib/superstore/types/time_type.rb
@@ -2,7 +2,8 @@ module Superstore
   module Types
     class TimeType < BaseType
       def encode(time)
-        raise ArgumentError.new("#{time.inspect} is not a Time") unless time.kind_of?(Time)
+        raise ArgumentError.new("#{time.inspect} does not respond to #to_time") unless time.is_a?(Time) || time.respond_to?(:to_time)
+        time = time.to_time unless time.is_a?(Time)
         time.utc.xmlschema(6)
       end
 
--- test/unit/types/time_type_test.rb
+++ test/unit/types/time_type_test.rb
@@ -3,6 +3,10 @@ require 'test_helper'
 class Superstore::Types::TimeTypeTest < Superstore::Types::TestCase
   test 'encode' do
     assert_equal '2004-12-24T01:02:03.000000Z', coder.encode(Time.utc(2004, 12, 24, 1, 2, 3))
+    assert_equal '2004-12-24T01:02:03.000000Z', coder.encode(DateTime.new(2004, 12, 24, 1, 2, 3))
+    assert_raise ArgumentError do
+      coder.encode 123
+    end
   end
 
   test 'decode' do
