321ae0247342649c6540ef40117c3ee167594db0
Matthew Higgins <developer@matthewhiggins.com>
2014-07-16 11:03:52 -0700

    Delete cassandra-cql override, now that latest cassandra-cql supports empty bind_vars

--- lib/superstore/connection.rb
+++ lib/superstore/connection.rb
@@ -1,20 +1,3 @@
-module CassandraCQL
-  class Statement
-    def self.sanitize(statement, bind_vars=[])
-      return statement if bind_vars.empty?
-
-      bind_vars = bind_vars.dup
-      expected_bind_vars = statement.count("?")
-
-      raise Error::InvalidBindVariable, "Wrong number of bound variables (statement expected #{expected_bind_vars}, was #{bind_vars.size})" if expected_bind_vars != bind_vars.size
-
-      statement.gsub(/\?/) do
-        quote(cast_to_cql(bind_vars.shift))
-      end
-    end
-  end
-end
-
 module Superstore
   module Connection
     extend ActiveSupport::Concern
