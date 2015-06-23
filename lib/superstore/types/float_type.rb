661e2360c8bd94bbd0d8a52ef054b65af642e279
Matthew Higgins <developer@matthewhiggins.com>
2015-06-23 09:04:00 -0700

    Properly typecast floats

--- lib/superstore/types/float_type.rb
+++ lib/superstore/types/float_type.rb
@@ -1,10 +1,7 @@
 module Superstore
   module Types
     class FloatType < BaseType
-      REGEX = /\A[-+]?\d+(\.\d+)?\Z/
       def encode(float)
-        raise ArgumentError.new("#{float.inspect} is not a Float") unless float.kind_of?(Float)
-
         if model.config[:adapter] == 'jsonb'
           float
         else
@@ -15,6 +12,10 @@ module Superstore
       def decode(str)
         return nil if str.empty?
         str.to_f
+      end
+
+      def typecast(value)
+        value.to_f
       end
     end
   end
