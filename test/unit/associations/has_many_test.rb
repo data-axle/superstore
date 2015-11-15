52a56291a24d4b69de6159f22bc9a3054929d78e
Matthew Higgins <developer@matthewhiggins.com>
2015-11-15 00:02:08 -0800

    fix test for has_one

--- test/unit/associations/has_many_test.rb
+++ test/unit/associations/has_many_test.rb
@@ -2,13 +2,12 @@ require 'test_helper'
 
 class Superstore::Associations::HasManyTest < Superstore::TestCase
   class TestObject < Issue
-    has_one :favorite_label, class_name: 'Label'
   end
 
   test 'has_many' do
     issue = TestObject.create!
     label = Label.create! name: 'important', issue_id: issue.id
 
-    assert_equal [label], issue.favorite_label
+    assert_equal [label], issue.labels
   end
 end
