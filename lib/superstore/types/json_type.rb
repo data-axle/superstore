d4ad010bf54414049c6e7956dd5bf9fd81ffd6fa
Matthew Higgins <developer@matthewhiggins.com>
2015-12-11 17:49:59 -0800

    Turns out to be faster to encode/decode than this slow loop

--- lib/superstore/types/json_type.rb
+++ lib/superstore/types/json_type.rb
@@ -1,6 +1,10 @@
 module Superstore
   module Types
     class JsonType < BaseType
+      OJ_OPTIONS = {mode: :compat}
+      def typecast(data)
+        Oj.load Oj.dump(data, OJ_OPTIONS)
+      end
     end
   end
 end
