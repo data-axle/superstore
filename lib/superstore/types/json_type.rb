298bbc7b4fd6a8e567f93e3ce9fe226a6f503157
Matthew Higgins <developer@matthewhiggins.com>
2015-12-11 18:20:40 -0800

    Remove cleverness with json type and dirty tracking. Waay to slow

--- lib/superstore/types/json_type.rb
+++ lib/superstore/types/json_type.rb
@@ -1,10 +1,6 @@
 module Superstore
   module Types
     class JsonType < BaseType
-      OJ_OPTIONS = {mode: :compat}
-      def typecast(data)
-        Oj.load Oj.dump(data, OJ_OPTIONS)
-      end
     end
   end
 end
--- test/unit/types/json_type_test.rb
+++ test/unit/types/json_type_test.rb
@@ -1,9 +1,4 @@
 require 'test_helper'
 
 class Superstore::Types::JsonTypeTest < Superstore::Types::TestCase
-  test 'typecast' do
-    assert_equal({'enabled' => false}, type.typecast('enabled' => false))
-    assert_equal({'born_at' => "2004-12-24T00:00:00.000Z"}, type.typecast('born_at' => Time.utc(2004, 12, 24)))
-    assert_equal(["2004-12-24T00:00:00.000Z"], type.typecast([Time.utc(2004, 12, 24)]))
-  end
 end
