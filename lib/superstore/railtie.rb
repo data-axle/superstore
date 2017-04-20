42542648991b14b4a5f29705120da134a52c2efa
Matthew Higgins <developer@matthewhiggins.com>
2017-04-19 21:40:29 -0700

    Set configurations before establish_connection

--- lib/superstore/railtie.rb
+++ lib/superstore/railtie.rb
@@ -2,6 +2,7 @@ module Superstore
   class Railtie < Rails::Railtie
     initializer "superstore.config" do |app|
       ActiveSupport.on_load :superstore do
+        self.configurations = Rails.application.config.database_configuration
         establish_connection
       end
     end
