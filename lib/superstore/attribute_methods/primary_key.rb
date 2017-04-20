5e24a8dff3f9c6c19a659165668451467dce6096
Matthew Higgins <developer@matthewhiggins.com>
2017-04-19 18:07:16 -0700

    call establish_connection in the railtie

--- lib/superstore/attribute_methods/primary_key.rb
+++ lib/superstore/attribute_methods/primary_key.rb
@@ -8,6 +8,10 @@ module Superstore
         def primary_key
           PRIMARY_KEY
         end
+
+        def quoted_primary_key
+          @quoted_primary_key ||= connection.quote_column_name(primary_key)
+        end
       end
 
       def id
@@ -20,10 +24,6 @@ module Superstore
 
       def attributes
         super.update(self.class.primary_key => id)
-      end
-
-      def quoted_primary_key
-        @quoted_primary_key ||= connection.quote_column_name(primary_key)
       end
     end
   end
