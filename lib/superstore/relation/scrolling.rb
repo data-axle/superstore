dca08c57ba6c21ee442ad88b76885a67d52ad30c
Matthew Higgins <developer@matthewhiggins.com>
2017-04-20 11:04:48 -0700

    Properly use instantiate

--- lib/superstore/relation/scrolling.rb
+++ lib/superstore/relation/scrolling.rb
@@ -4,8 +4,8 @@ module Superstore
       def scroll_each(options = {})
         batch_size = options[:batch_size] || 1000
 
-        klass.adapter.scroll(self, batch_size) do |key, attributes|
-          yield klass.instantiate(key, attributes)
+        klass.adapter.scroll(self, batch_size) do |attributes|
+          yield klass.instantiate(attributes)
         end
       end
 
