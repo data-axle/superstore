12de7f33c4817d2031a15385a104b93f655fe042
Dan Cromer <cromer.dan@gmail.com>
2018-03-27 16:54:59 -0700

    Handles DateType#decode ArgumentError
    If the value is invalid, returns nil instead.  This is consistent with
    TimeType.

--- test/unit/types/date_type_test.rb
+++ test/unit/types/date_type_test.rb
@@ -6,11 +6,10 @@ class Superstore::Types::DateTypeTest < Superstore::Types::TestCase
   end
 
   test 'decode' do
+    assert_nil type.decode('')
+    assert_nil type.decode('nil')
+    assert_nil type.decode('bad format')
     assert_equal Date.new(2004, 4, 25), type.decode('2004-04-25')
     assert_equal Date.new(2017, 5, 1), type.decode('2017-05-01T21:39:06.796897Z')
-  end
-
-  test 'decoding a blank dates' do
-    assert_nil type.decode('')
   end
 end
