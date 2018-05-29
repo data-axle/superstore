a2f4c99e37382f095bbaa4ce9389d570a8b3fca8
Matthew Higgins <developer@matthewhiggins.com>
2018-05-28 19:00:06 -0700

    No reason to encode int

--- lib/superstore/types/integer_type.rb
+++ lib/superstore/types/integer_type.rb
@@ -1,12 +1,6 @@
 module Superstore
   module Types
     class IntegerType < BaseType
-      def encode(int)
-        raise ArgumentError.new("#{int.inspect} is not an Integer.") unless int.kind_of?(Integer)
-
-        int
-      end
-
       def decode(str)
         str.to_i unless str.empty?
       end
