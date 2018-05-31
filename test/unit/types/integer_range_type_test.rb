ccc85b745b13b4e6c393d9467c8ef6336aa67e47
Matthew Higgins <developer@matthewhiggins.com>
2018-05-31 16:57:48 +0700

    The interface

--- test/unit/types/integer_range_type_test.rb
+++ test/unit/types/integer_range_type_test.rb
@@ -7,13 +7,15 @@ class Superstore::Types::IntegerRangeTypeTest < Superstore::Types::TestCase
     assert_equal [nil, 5], type.encode(-Float::INFINITY..5)
   end
 
-  # test 'decode' do
-  #   assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.decode(["2004-04-25", "2004-05-15"])
-  # end
+  test 'decode' do
+    # assert_equal [4, 5], type.encode(4..5)
+    # assert_equal [4, nil], type.encode(4..Float::INFINITY)
+    # assert_equal [nil, 5], type.encode(-Float::INFINITY..5)
+  end
   #
-  # test 'typecast' do
-  #   assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.typecast(Date.new(2004, 4, 25)..Date.new(2004, 5, 15))
-  #   assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.typecast([Date.new(2004, 4, 25), Date.new(2004, 5, 15)])
-  #   assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.typecast(["2004-04-25", "2004-05-15"])
-  # end
+  test 'typecast' do
+    assert_equal 1..5, type.typecast(1..5)
+    assert_equal 1..5, type.typecast([1, 5])
+    assert_equal 1..Float::INFINITY, type.typecast([1, nil])
+  end
 end
