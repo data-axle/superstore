847b1ae394873fa52219d1da6534a6facc3e154e
Matthew Higgins <developer@matthewhiggins.com>
2018-05-31 12:48:25 +0700

    functioning tests

--- test/unit/types/date_range_type_test.rb
+++ test/unit/types/date_range_type_test.rb
@@ -2,16 +2,12 @@ require 'test_helper'
 
 class Superstore::Types::DateRangeTypeTest < Superstore::Types::TestCase
   test 'encode' do
-    assert_equal '2004-04-25', type.encode(Date.new(2004, 4, 25) .. Date.new(2004, 5, 15))
+    assert_equal ["2004-04-25", "2004-05-15"], type.encode(Date.new(2004, 4, 25) .. Date.new(2004, 5, 15))
   end
-
-  # test 'decode' do
-  #   assert_nil type.decode('')
-  #   assert_nil type.decode('nil')
-  #   assert_nil type.decode('bad format')
-  #   assert_equal Date.new(2004, 4, 25), type.decode('2004-04-25')
-  #   assert_equal Date.new(2017, 5, 1), type.decode('2017-05-01T21:39:06.796897Z')
-  # end
+  #
+  test 'decode' do
+    assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.decode(["2004-04-25", "2004-05-15"])
+  end
   #
   # test 'typecast' do
   #   assert_nil type.typecast(1000)
