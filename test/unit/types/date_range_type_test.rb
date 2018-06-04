531cd512fb283271c1efc1970c9236913d2cf2f4
Dan Cromer <cromer.dan@gmail.com>
2018-06-04 13:23:55 -0700

    Test edge cases around date ranges

--- test/unit/types/date_range_type_test.rb
+++ test/unit/types/date_range_type_test.rb
@@ -3,17 +3,19 @@ require 'test_helper'
 class Superstore::Types::DateRangeTypeTest < Superstore::Types::TestCase
   test 'encode' do
     assert_equal ["2004-04-25", "2004-05-15"], type.encode(Date.new(2004, 4, 25) .. Date.new(2004, 5, 15))
+    assert_equal ["2004-04-25", "2004-05-15"], type.encode(Date.new(2004, 5, 15) .. Date.new(2004, 4, 25))
   end
-  #
+
   test 'decode' do
     assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.decode(["2004-04-25", "2004-05-15"])
   end
-  #
+
   test 'typecast' do
     assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.typecast(Date.new(2004, 4, 25)..Date.new(2004, 5, 15))
     assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.typecast([Date.new(2004, 4, 25), Date.new(2004, 5, 15)])
     assert_equal Date.new(2004, 4, 25)..Date.new(2004, 5, 15), type.typecast(["2004-04-25", "2004-05-15"])
 
+    assert_nil type.typecast(["2004-04-25", nil])
     assert_nil type.typecast([nil, "2004-05-15"])
     assert_nil type.typecast(["xx", "2004-05-15"])
   end
