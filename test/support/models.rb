a2ebcde9a45b1c6d48ca351aa3e1c99ec24ace05
Dan Cromer <cromer.dan@gmail.com>
2018-06-07 09:22:47 -0700

    Revert "Add test that illustrates as_json problem"
    This reverts commit 1ce8ef0220a634d7b32d4d8bc9d95a20d312e5ba.

--- test/support/models.rb
+++ test/support/models.rb
@@ -9,7 +9,6 @@ class Issue < Superstore::Base
   string :description
   string :title
   string :parent_issue_id
-  integer_range :skill_level_required
   json :comments
 
   before_create { self.description ||= 'funny' }
--- test/unit/serialization_test.rb
+++ test/unit/serialization_test.rb
@@ -2,11 +2,8 @@ require 'test_helper'
 
 class Superstore::SerializationTest < Superstore::TestCase
   test 'as_json' do
-    issue = Issue.new(skill_level_required: 100..200)
-    expected = {
-      "id" => issue.id,
-      "skill_level_required" => [100, 200]
-    }
+    issue = Issue.new
+    expected = {"id" => issue.id}
 
     assert_equal expected, issue.as_json
   end
