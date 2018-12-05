4878858e31899c71aff0f3a4b84b9db25cc42153
Alex Mooney <Alex.Mooney@infogroup.com>
2018-12-05 11:24:24 -0800

    Change integer_type_test for AM::Type

--- test/unit/types/integer_type_test.rb
+++ test/unit/types/integer_type_test.rb
@@ -2,11 +2,14 @@ require 'test_helper'
 
 class Superstore::Types::IntegerTypeTest < Superstore::Types::TestCase
   test 'typecast' do
-    assert_nil type.typecast('')
-    assert_nil type.typecast('abc')
-    assert_equal 3, type.typecast(3)
-    assert_equal 3, type.typecast('3')
-    assert_equal(-3, type.typecast('-3'))
-    assert_equal 27, type.typecast('027')
+    assert_nil type.typecast('my_int', '').value
+    assert_nil type.typecast('my_int', 'abc').value
+    assert_equal 3, type.typecast('my_int', 3).value
+    assert_equal 3, type.typecast('my_int', '3').value
+    assert_equal(-3, type.typecast('my_int', '-3').value)
+    assert_equal 27, type.typecast('my_int', '027').value
+    assert_equal 'my_int', type.typecast('my_int', 3).name
+
+    assert type.typecast('my_int', 3).type.is_a? ActiveModel::Type::Integer
   end
 end
