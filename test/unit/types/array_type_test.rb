9dda51213e2ba8066aa8e9d0978b2af94fb0138f
Matthew Higgins <developer@matthewhiggins.com>
2019-01-21 15:26:33 -0800

    Fix test

--- test/unit/types/array_type_test.rb
+++ test/unit/types/array_type_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class Superstore::Types::ArrayTypeTest < Superstore::Types::TestCase
   test 'typecast' do
     assert_equal ['x', 'y'], type.typecast(['x', 'y'].to_set)
-    assert_nil type.typecast('x')
+    assert_equal ['x'], type.typecast('x')
   end
 end
