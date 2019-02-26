aa333f7e025fe998bec487b138772774df1f8c2b
Matthew Higgins <developer@matthewhiggins.com>
2019-02-26 15:04:15 -0800

    Remove commented code

--- lib/superstore/base.rb
+++ lib/superstore/base.rb
@@ -5,37 +5,14 @@ require 'superstore/types'
 module Superstore
   class Base < ActiveRecord::Base
 
-    # extend ActiveModel::Naming
-    # include ActiveModel::Conversion
-    # extend ActiveSupport::DescendantsTracker
-    # include ActiveModel::Serializers::JSON
-    # include GlobalID::Identification
-
-    # extend ActiveRecord::ConnectionHandling
     self.connection_specification_name = 'primary'
-    # extend ActiveRecord::Querying
-    # extend ActiveRecord::Translation
-    # extend ActiveRecord::Delegation::DelegateCache
-    # include ActiveRecord::Core
     include Persistence
-    # include ActiveRecord::ReadonlyAttributes
     include ModelSchema
-    # include Inheritance
-    # include ActiveRecord::Scoping
-    # include ActiveRecord::Sanitization
     include AttributeAssignment
-    # include ActiveRecord::Integration
-    # include ActiveRecord::Validations
     include Attributes
-    # include ActiveRecord::AttributeDecorators
-    # include ActiveRecord::DefineCallbacks
     include AttributeMethods
-    # include ActiveRecord::Callbacks
     include Timestamp
     include Associations
-    # include ActiveRecord::AutosaveAssociation
-    # include ActiveRecord::Reflection
-    # include ActiveRecord::Transactions
 
     include Core
     include Connection
