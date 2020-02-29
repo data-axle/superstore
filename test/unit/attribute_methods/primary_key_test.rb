44dfbdb7a15757577bd13341c37f658129ac9dda
Jeremy Wilmot <jeremy.wilmot@infogroup.com>
2020-02-29 10:28:37 -0800

    Add more missing primary key tests

--- test/unit/attribute_methods/primary_key_test.rb
+++ test/unit/attribute_methods/primary_key_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class Superstore::AttributeMethods::PrimaryKeyTest < Superstore::TestCase
   test 'get id' do
     model = temp_object do
-      has_id
       key do
         "foo"
       end
@@ -13,16 +12,44 @@ class Superstore::AttributeMethods::PrimaryKeyTest < Superstore::TestCase
     assert_equal 'foo', record.id
   end
 
+  test 'get id without primary key' do
+    model = Class.new(Superstore::Base) do
+      attribute :name, type: :string
+    end
+    record = model.new(name: 'John')
+
+    assert_nil record.id
+  end
+
   test 'set id' do
     issue = Issue.new id: 'foo'
 
     assert_equal 'foo', issue.id
   end
 
+  test 'set id without primary key' do
+    model = Class.new(Superstore::Base) do
+      attribute :name, type: :string
+    end
+
+    assert_raises ActiveModel::MissingAttributeError do
+      model.new(id: 'id')
+    end
+  end
+
   test 'attributes' do
     issue = Issue.new(id: 'lol')
 
     assert_not_nil issue.attributes['id']
+  end
+
+  test 'attributes without primary key' do
+    model = Class.new(Superstore::Base) do
+      attribute :name, type: :string
+    end
+    record = model.new(name: 'John')
+
+    assert_nil record.attributes['id']
   end
 
   test 'has_primary_key?' do
