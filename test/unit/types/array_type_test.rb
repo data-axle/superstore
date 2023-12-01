f7a0f3774184acfc67c363cf56f0595f4a5aaa6a
Matthew Hinea <matthew.hinea@gmail.com>
2023-11-30 16:17:25 -0800

    Coerce empty arrays to `nil`

--- test/unit/types/array_type_test.rb
+++ test/unit/types/array_type_test.rb
@@ -4,6 +4,6 @@ class Superstore::Types::ArrayTypeTest < Superstore::Types::TestCase
   test 'cast_value' do
     assert_equal ['x', 'y'], type.cast_value(['x', 'y'].to_set)
     assert_equal ['x'], type.cast_value('x')
-    assert_nil type.cast_value('x')
+    assert_nil type.cast_value(nil)
   end
 end
