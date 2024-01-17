78afac1116d744caa9f2c2b5630668a2af0fd062
Matthew Hinea <matthew.hinea@gmail.com>
2024-01-16 16:45:53 -0800

    JSON: serialize empty arrays as `nil`

--- lib/superstore/types/json_type.rb
+++ lib/superstore/types/json_type.rb
@@ -1,6 +1,9 @@
 module Superstore
   module Types
     class JsonType < Base
+      def serialize(value)
+        value if value.present?
+      end
     end
   end
 end
--- test/unit/types/json_type_test.rb
+++ test/unit/types/json_type_test.rb
@@ -1,4 +1,20 @@
 require 'test_helper'
 
 class Superstore::Types::JsonTypeTest < Superstore::Types::TestCase
+  test 'serializes empty array to nil' do
+    comments = { 'foo' => 'bar' }
+    issue1   = Issue.new(comments:)
+    issue2   = Issue.new(comments: [])
+    issue3   = Issue.new(comments: {})
+
+    assert_equal comments, issue1.comments
+    assert_equal [], issue2.comments
+    assert_equal({}, issue3.comments)
+
+    [issue1, issue2].each(&:save!).each(&:reload)
+
+    assert_equal comments, issue1.comments
+    assert_nil issue2.comments
+    assert_equal({}, issue3.comments)
+  end
 end
